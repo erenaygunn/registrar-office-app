81c16cad1523aaa66981689247f06e0557dcea97
erenaygunn <117078263+erenaygunn@users.noreply.github.com>
2022-12-09 11:57:14 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ while flag:
     action = int(input("Enter the digit of action you want to perform: "))
     print()
 
-    if action == 1:
+    if action == 1:  # Listing courses
         print("Course Code |  Course Name")
         print("-----------------------------")
 
@@ -31,13 +31,14 @@ while flag:
         courses.close()  # Closes the file so that it can be re-opened after this if statement.
         print()
 
-        act = int(input("0:Go back. \n"
-                        "1:Exit. \n"
-                        "Choose action:"))
+        act = int(input("0:Go back. \n"  # This act menu helps current action to keep running,
+                        "1:Exit. \n"  # It has its own flag as 'act' variable, user selects actions.
+                        "Choose action:"))  # It will be used multiple times in this program.
+
         if act == 1:
             flag = False
 
-    elif action == 2:
+    elif action == 2:  # Listing courses with at least one student
         print("Course Code |      Course Name            | Student Count")
         print("--------------------------------------------------------")
         for line in courses:
@@ -50,9 +51,9 @@ while flag:
         print()
         courses.close()
 
-        act = int(input("0:Go back. \n"     # This act menu helps current action to keep running,
-                        "1:Exit. \n"            # It has its own flag as 'act' variable, user selects actions.
-                        "Choose action:"))      # It will be used multiple times in this program.
+        act = int(input("0:Go back. \n"
+                        "1:Exit. \n"
+                        "Choose action:"))
         if act == 1:
             flag = False
 
@@ -71,12 +72,12 @@ while flag:
 
             act = int(input("0:Go back. \n"
                             "1:Exit. \n"
-                            "2:Add another course. \n"   
+                            "2:Add another course. \n"
                             "Choose action:"))
             if act == 1:
                 flag = False
 
-    elif action == 4:
+    elif action == 4:  # Search course by code
         act = 2
 
         while act == 2:
@@ -87,9 +88,10 @@ while flag:
                 for line in courses:
                     newline = line.strip("\n")
                     items = newline.split(";")
-                    if items[0] == code:            # Searches the course with the code user entered.
-                        print(items[0], ":", items[1], " / Instructor: ", items[2], " /  Students: ", items[3], "\n")
-                        exist = True            # If this is not set true, it will print that course doesn't exist.
+                    if items[0] == code:  # Searches the course with the code user entered.
+                        print(items[0], ":", items[1], " / Instructor: ", items[2], " /  Students: ", items[3],
+                              "\n")
+                        exist = True  # If this is not set true, it will print that course doesn't exist.
                         break
 
                 if not exist:
@@ -101,7 +103,7 @@ while flag:
             if act == 1:
                 flag = False
 
-    elif action == 5:
+    elif action == 5:  # Search course by name
         act = 2
 
         while act == 2:
@@ -127,30 +129,72 @@ while flag:
                 if act == 1:
                     flag = False
 
-    elif action == 6:
+    elif action == 6:  # Register student to course
         act = 2
 
         while act == 2:
+            course_exist = False
+            student_exist = False
             code = input("Enter course code: ")
             st_id = input("Enter student id: ")
 
-            with open("./files/student.txt", "r") as students, open("./files/course.txt", "r") as courses:
-                data = courses.readlines()
-                print(data)
-                # for line in courses:
-                #     newline = line.strip("\n")
-                #     items = newline.split(";")
-                #     if code == items[0]:
-                #         with open("./files/course.txt", "w") as f:
-                #             line
-                #
-                # for line in students:
-                #     newline = line.strip("\n")
-                #     items = newline.split(";")
-                #     if st_id == items[0]:
-                #         student_exist = True
+            # This increases course's student count by 1
+            with open("./files/course.txt", "r") as f:
+                newline = []  # This will store modified text file
+                for course_line in f.readlines():
+                    new_course = course_line.strip("\n")
+                    elements = new_course.split(";")
 
-    elif action == 7:
+                    if elements[0] == code:  # Checks course code
+                        newline.append(
+                            course_line.replace(elements[3], str(int(elements[3]) + 1)))  # Increases count
+                        course_exist = True
+
+                    else:
+                        newline.append(course_line)
+
+            with open("./files/course.txt", "w") as f:
+                for line in newline:  # Writes entire modified text in file
+                    f.writelines(line)
+
+            # This adds course code at the end of the student's information.
+            with open("./files/student.txt", "r") as f:
+                newline = []
+                for student in f.readlines():
+                    new_student = student.strip("\n")
+                    elements = new_student.split(";")
+
+                    if elements[0] == st_id:  # Checks student id.
+                        newline.append(student.strip("\n") + "," + code + "\n")  # If it is same, adds course
+                        student_exist = True
+
+                    else:
+                        newline.append(student)
+
+            with open("./files/student.txt", "w") as f:
+                for line in newline:  # Writes entire modified text again
+                    f.writelines(line)
+
+            if student_exist and not course_exist:
+                print("Course doesn't exist!\n")
+
+            elif not student_exist and course_exist:
+                print("Student doesn't exist!\n")
+
+            elif student_exist and course_exist:
+                print("Student successfully registered.\n")
+
+            else:
+                print("Course and student not found. Try again.\n")
+
+            act = int(input("0:Go back. \n"
+                            "1:Exit. \n"
+                            "2:Register another student. \n"
+                            "Choose action:"))
+            if act == 1:
+                flag = False
+
+    elif action == 7:  # Lists students and their courses
         act = 2
 
         while act == 2:
@@ -167,16 +211,16 @@ while flag:
             if act == 1:
                 flag = False
 
-    if action == 8:
-        items = courses.read().split("\n")      # Separates each line into a list element
+    elif action == 8:  # List top 3 crowded courses
+        items = courses.read().split("\n")  # Separates each line into a list element
 
         sorted_items = []
-        for item in items:                         # This creates nested list so that student count can be accessed.
+        for item in items:  # This creates nested list so that student count can be accessed.
             sorted_items.append(item.split(";"))
 
         # This loop sorts courses depending on student count.
         for i in range(len(sorted_items)):
-            for j in range(i+1, len(sorted_items)):
+            for j in range(i + 1, len(sorted_items)):
                 if int(sorted_items[i][3]) > int(sorted_items[j][3]):
                     sorted_items[i], sorted_items[j] = sorted_items[j], sorted_items[i]
         print(f"1. {sorted_items[-1][1]}: {sorted_items[-1][3]} students \n"
@@ -189,7 +233,7 @@ while flag:
         if act == 1:
             flag = False
 
-    elif action == 9:
+    elif action == 9:  # List top 3 students with most courses
         items = students.read().split("\n")  # Separates each line into a list element
 
         sorted_items = []
@@ -197,8 +241,15 @@ while flag:
             sorted_items.append(item.split(";"))
 
         for i in sorted_items:
-            course_count = len(i[2].split(","))     # This calculates the course count of each student
-            i.append(str(course_count))             # Adds course count to the nested list
+            try:
+                course_count = len(i[2].split(","))  # This calculates the course count of each student
+                i.append(str(course_count))  # Adds course count to the nested list
+
+            # This exception is for students with no registered courses.
+            except IndexError:
+                i.append(str(0))
+                i.append(str(0))
+                i.append(str(0))
 
         # This loop sorts students depending on course counts.
         for i in range(len(sorted_items)):
@@ -215,5 +266,7 @@ while flag:
         if act == 1:
             flag = False
 
-    if action == 0:
+    elif action == 0:
         flag = False
+    else:
+        print("Please choose between the given numbers only.\n")
